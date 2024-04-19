b4f85adc196e0eef44cb786a192ac331df3118b8
Daffeyd Wilbert <daffeydwilbert@gmail.com>
2024-04-19 09:05:10 +0700

    Handle Scroll Hero Button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,15 @@
-"use client"
+"use server"
 import Image from "next/image";
 import CustomButton from "./CustomButton";
 
 const hero = () => {
-  const handleScroll = () => {};
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
+
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -22,8 +28,13 @@ const hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-            <Image src="/hero.png" alt="Hero Image" fill className="object-contain"/>
-            <div className="hero__image-overlay"/>
+          <Image
+            src="/hero.png"
+            alt="Hero Image"
+            fill
+            className="object-contain"
+          />
+          <div className="hero__image-overlay" />
         </div>
       </div>
     </div>
